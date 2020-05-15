f58b05fcead978871a67f4a16f0f17e2be123ba9
Artem Didenko <mikrafizik@gmail.com>
2020-05-15 18:02:54 +0300

    init github action

---
+++ README.md
@@ -0,0 +1,52 @@
+# GitHub Release Manager
+
+A Github Action to automatically bump tag version and generate release on your masters branch, on merge, with the latest SemVer formatted version.
+
+## Usage
+
+```yaml
+# This is a basic workflow to help you get started with Actions
+
+name: Release creator
+
+# Controls when the action will run. Triggers the workflow on push or pull request merge
+# events but only for the master branch
+on:
+  push:
+    branches: [ master ]
+jobs:
+  manage_release_job:
+    runs-on: ubuntu-latest
+    name: Generate release
+    steps:
+    - name: Generate release
+      id: release
+      uses: mikrafizik/releaseManager@master
+      with:
+        # secret github token, required
+        github_token: "${{ secrets.GITHUB_TOKEN }}"
+    # Use the output from the `release` step
+    - name: Get the output time
+      run: echo "Execution time ${{ steps.release.outputs.time }}"
+```
+
+### Input
+
+- **github_token** _(required)_ - Required for permission to tag the repo. Usually `${{ secrets.GITHUB_TOKEN }}`.
+- **bump_type** _(optional)_ - Which type of bump to use. [Available types](#bump) (default: `patch`).
+
+### Outputs
+
+- **new_tag** - The value of the newly created tag. Note that if there hasn't been any new commit, this will be `undefined`.
+- **time** - The value of the time when Action was executed.
+
+### Bump
+Available bump types:
+- major (BC)
+- minor
+- patch
+
+The action will parse the new commits since the last tag using pattern
+```#release-{BumpType}```
+
+If no commit message contains any information, then **default_bump** will be used.
---
+++ index.js
@@ -0,0 +1,183 @@
+const {context, GitHub} = require('@actions/github');
+const core = require('@actions/core');
+
+async function run() {
+    try {
+        const token = core.getInput('github_token', {required: true});
+        const repo = context.repo.repo;
+        const owner = context.repo.owner;
+        const time = (new Date()).toISOString();
+
+        const {GITHUB_REF, GITHUB_SHA} = process.env;
+        if (!GITHUB_REF) {
+            core.setFailed("Missing GITHUB_REF");
+            return;
+        }
+
+        if (!GITHUB_SHA) {
+            core.setFailed("Missing GITHUB_SHA");
+            return;
+        }
+
+        const octokit = new GitHub(token);
+
+        core.debug(`Start generating tag`);
+        const {lastTag, newTag, preRelease} = await generateTag(octokit, repo, owner);
+
+        core.debug(`Start fetching commits`);
+        // TODO pagination and filters
+        const {data: commits} = await octokit.repos.listCommits({
+            owner,
+            repo,
+        });
+
+        const message = getMessage(commits, lastTag);
+
+        // throw new Error('SHIT IS GOING HERE');
+        core.debug(`Creating Tag: ${newTag}`);
+        const tag = await octokit.git.createTag({
+            ...context.repo,
+            tag: newTag,
+            object: GITHUB_SHA,
+            message: message,
+            type: 'commit',
+            tagger: {
+                name: context.payload.pusher.name,
+                email: context.payload.pusher.email,
+                date: time,
+            }
+        });
+
+        core.debug(`Creating reference for Tag: ${newTag}`);
+        await octokit.git.createRef({
+            ...context.repo,
+            ref: `refs/tags/${newTag}`,
+            sha: tag.data.sha,
+        });
+
+        core.debug(`Creating release with Tag: ${newTag}`);
+        await octokit.repos.createRelease({
+            owner,
+            repo,
+            tag_name: newTag,
+            name: newTag,
+            body: message,
+            prerelease: preRelease,
+        });
+
+        core.setOutput("new_tag", newTag);
+        core.setOutput("time", time);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+function getMessage(commits, lastTag) {
+    const commitMessages = [];
+    for (let i = 0; i < commits.length; i++) {
+        const c = commits[i];
+        if (c.sha === lastTag.commit.sha) {
+
+            break;
+        }
+        const message = c.commit.message;
+
+        if (message.startsWith('Merge', 0)) {
+            continue;
+        }
+
+        commitMessages.push('#### ' + message);
+    }
+
+    return commitMessages.join("\n");
+}
+
+function bumpVersionTokens(tokens, type = null) {
+    if (!tokens instanceof Array) {
+        throw new Error('Bumps only array tokens');
+    }
+
+    if (typeof tokens[0] == "undefined") {
+        throw new Error('Major token undefined')
+    }
+
+    if (typeof tokens[1] == "undefined") {
+        tokens[1] = 0;
+    }
+
+    if (typeof tokens[2] == "undefined") {
+        tokens[2] = 0;
+    }
+
+    const defaultType = core.getInput('bump_type');
+    if (!['major', 'minor', 'patch'].includes(defaultType)) {
+        throw new Error('Invalid default bump type')
+    }
+
+    switch (type) {
+        case 'major':
+            tokens[0] = 1 + parseInt(tokens[0]);
+            tokens[1] = 0;
+            tokens[2] = 0;
+
+            break;
+        case 'minor':
+            tokens[1] = 1 + parseInt(tokens[1]);
+            tokens[2] = 0;
+
+            break;
+        case 'patch':
+            tokens[2] = 1 + parseInt(tokens[2]);
+
+            break;
+        default:
+            return bumpVersionTokens(tokens, defaultType);
+    }
+
+    return tokens;
+}
+
+async function generateTag(octokit, repo, owner) {
+    const {data: listTags} = await octokit.repos.listTags({
+        owner,
+        repo,
+    });
+
+    let lastTag = listTags[0];
+    //init first tag
+    if (!lastTag) {
+        lastTag = {
+            name: 'v0.0.0',
+            commit: {
+                sha: null,
+            }
+        };
+    }
+
+    let dirtname = lastTag.name;
+    if (dirtname.startsWith('v', 0)) {
+        //removing 'v' symbol
+        dirtname = dirtname.substring(1);
+    }
+
+    let bumpType = context.payload.head_commit.message.match(/\#release-\w+/gm);
+    if (bumpType instanceof Array) {
+        //remove #release-  part
+        bumpType = bumpType[0].substring(9);
+    }
+
+    const tokens = bumpVersionTokens(dirtname.split('.'), bumpType);
+    const newTag = 'v' + tokens.join('.');
+
+    // check first symbol after v in
+    const preRelease = '0' === tokens[0];
+
+    return {
+        preRelease,
+        newTag,
+        lastTag,
+    }
+}
+
+run();
+
